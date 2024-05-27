6e4439484d755e1b160ab54a15c52d98a517a92b
Jun Siang Cheah <git@jscheah.me>
2024-05-27 21:57:40 +0100

    add fallback that just takes length/4

--- src/lib/workers/tokenizer.worker.ts
+++ src/lib/workers/tokenizer.worker.ts
@@ -10,27 +10,46 @@ if (!caches || dev) {
 	env.customCache = new Cache('transformers-cache');
 }
 
+interface FallbackTokenizer {
+	countTokens: (text: string) => number;
+}
+
 // This is a map of all the tokenizer instances that we have loaded.
 // model_id -> promise that resolves to tokenizer
-const TOKENIZER_MAPPINGS: Map<string, Promise<PreTrainedTokenizer>> = new Map();
+const TOKENIZER_MAPPINGS: Map<string, Promise<PreTrainedTokenizer | FallbackTokenizer>> = new Map();
 
 // Listen for messages from the main thread
 self.onmessage = async (event: MessageEvent<TokenizerWorkerMessage>) => {
 	let tokenizerPromise = TOKENIZER_MAPPINGS.get(event.data.modelId);
 	// Load the tokenizer if it hasn't been loaded yet
 	if (!tokenizerPromise) {
-		tokenizerPromise = AutoTokenizer.from_pretrained(event.data.modelId);
+		if (event.data.modelId == 'fallback') {
+			tokenizerPromise = new Promise((resolve) => {
+				const tokenizer: FallbackTokenizer = {
+					countTokens: (text: string) => text.length / 4
+				};
+				resolve(tokenizer);
+			});
+		} else {
+			tokenizerPromise = AutoTokenizer.from_pretrained(event.data.modelId);
+		}
 		TOKENIZER_MAPPINGS.set(event.data.modelId, tokenizerPromise);
 	}
 
 	const tokenizer = await tokenizerPromise;
 	const text = event.data.text;
-	const token_ids = tokenizer.encode(text);
+	let tokenLength: number;
+	if (tokenizer instanceof PreTrainedTokenizer) {
+		const token_ids = tokenizer.encode(text);
+		tokenLength = token_ids.length;
+	} else {
+		tokenLength = tokenizer.countTokens(text);
+	}
 
 	const response: TokenizerWorkerResponse = {
 		modelId: event.data.modelId,
 		text: text,
-		length: token_ids.length
+		length: tokenLength
 	};
 
 	// Send the output back to the main thread
